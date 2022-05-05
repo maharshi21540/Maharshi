8b4283c1f93da6496f0533189ec41e6d41f8ddb6
maharshi21540 <105012822+maharshi21540@users.noreply.github.com>
2022-05-05 19:45:50 +0100

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,6 @@
+
+console.log("hello Maharshi");
+console.log("Welcome to VIsual Code Studio ");
+console.log("Lets get you started");
+console.log("This is a wonderful app to write codes");
+console.log("So what are you waiting for lets begin");
